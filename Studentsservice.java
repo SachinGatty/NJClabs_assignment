a7665dad1c1fcd8dff7b0a7d2b7cf1ccef855f6c
SachinGatty <75385222+SachinGatty@users.noreply.github.com>
2020-12-03 17:35:48 +0530

    Create Studentsservice.java

---
+++ Studentsservice.java
@@ -0,0 +1,26 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.student;
+import com.example.demo.repository.studentRepo;
+
+@Service
+public class studentservice {
+	@Autowired
+	 private studentRepo stud;
+	
+	public List<student> getAllStud() {
+		
+			List<student> studList = stud.findAll();
+			return studList;
+	}
+	
+	public student addStudent(student data) {
+		student studData = stud.save(data);
+		return studData;
+}
+}
