a5f12ecb3d6d9a3026da808bc75cb3eb7b2d21d7
SachinGatty <75385222+SachinGatty@users.noreply.github.com>
2020-12-03 17:35:18 +0530

    Create StudentRepo.java

---
+++ StudentRepo.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.student;
+
+
+@Repository
+public interface studentRepo extends CrudRepository<student, Long> {
+	public List<student> findAll();
+	public student save(student studentDetails);
+}
