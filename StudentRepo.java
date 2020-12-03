75a47acbfb515457d6e1817904d0d55a39703416
SachinGatty <75385222+SachinGatty@users.noreply.github.com>
2020-12-03 17:43:57 +0530

    Update StudentRepo.java

--- StudentRepo.java
+++ StudentRepo.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.student;
 
-
+//Repository to storage and retrieval instead od select*
 @Repository
 public interface studentRepo extends CrudRepository<student, Long> {
 	public List<student> findAll();
